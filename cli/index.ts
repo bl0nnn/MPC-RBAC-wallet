47903da72204e33cf57d15d318210c03a669bee3
miguel <miguel@Mac.station>
2026-02-15 18:32:16 +0100

    cli refining

--- cli/index.ts
+++ cli/index.ts
@@ -15,9 +15,109 @@ import {
   deposit,
   signMessage,
 } from "./rbac/rbac.ts";
+<<<<<<< HEAD
 
 const rl = readline.createInterface({ input, output });
 
+=======
+import { bcs } from "@mysten/sui/bcs";
+import { fromBase64, isValidSuiAddress } from "@mysten/sui/utils";
+import { getSuiClient } from "./config/clients.ts";
+import { ENV } from "./config/env.ts";
+
+const suiClient = await getSuiClient();
+
+const rl = readline.createInterface({ input, output });
+
+const UID = bcs.Address;
+const ID = bcs.Address;
+const walletId = ENV.WALLET_ADDRESS;
+
+const Table = bcs.struct("Table", {
+  id: bcs.Address,
+  size: bcs.u64(),
+});
+
+function VecMap(keyType: any, valueType: any) {
+  return bcs.vector(
+    bcs.struct("VecMapEntry", {
+      key: keyType,
+      value: valueType,
+    })
+  );
+}
+
+const RoleConfig = bcs.struct("RoleConfig", {
+  sign_ability: bcs.bool(),
+  spending_limit: bcs.u64(),
+  recovery_time: bcs.u64(),
+});
+
+const RecoveryRequest = bcs.struct("RecoveryRequest", {
+  new_admin: bcs.Address,
+  execute_after: bcs.u64(),
+});
+
+const WalletBCS = bcs.struct("Wallet", {
+  id: UID,
+
+  current_admin: bcs.Address,
+
+  dWallets: Table,
+
+  users: Table,
+
+  roles_config: VecMap(
+    bcs.u8(),
+    RoleConfig
+  ),
+
+  active_recovery: bcs.option(RecoveryRequest),
+
+  dwallet_network_encryption_key_id: ID,
+
+  presignatures: Table,
+
+  ikas: bcs.u64(),
+
+  suis: bcs.u64(),
+});
+
+async function getWallet() {
+  const object = await suiClient.getObject({
+    id: walletId,
+    options: { showBcs: true },
+  });
+
+  const bytes = object.data?.bcs?.bcsBytes;
+  if (!bytes) throw new Error("No BCS");
+
+  return WalletBCS.parse(fromBase64(bytes));
+}
+
+async function getCurrentUsers(currentUsersId: string){
+  const currentUsersList = await suiClient.getDynamicFields({
+    parentId: currentUsersId,
+  });
+
+  const userAddrs: string[] = [];
+  for (const item of currentUsersList.data) {
+
+    const tableValue = await suiClient.getDynamicFieldObject({
+      parentId: currentUsersId,
+      name: {
+        type: item.name.type,
+        value: item.name.value
+      }
+    });
+    const useraddr = tableValue.data?.content?.fields.name
+    userAddrs.push(useraddr);
+  }
+
+  return userAddrs
+}
+
+>>>>>>> f8ca49f (cli refining)
 async function ask(q: string): Promise<string> {
   return (await rl.question(q)).trim();
 }
@@ -70,14 +170,70 @@ async function main() {
       switch (choice) {
         case "1": {
           const chain = await askChain();
+<<<<<<< HEAD
           const role_ids = parseNumberArray(await ask("role_ids: "));
           const sign = parseBoolArray(await ask("roles_sign_ability: "));
           const spending = parseNumberArray(await ask("spending_limit: "));
+=======
+
+          let role_ids: number[] = [];
+          while(true){
+            role_ids = parseNumberArray(await ask("role_ids (comma separated, no space): "));
+            
+            let flag = 0;
+
+            for(let i = 0; i < role_ids.length; i++){
+
+              if(role_ids[i] <= 0 || isNaN(role_ids[i])){
+                flag = 1;
+                break
+              }
+
+              for(let j = i+1; j < role_ids.length; j++){
+                if(role_ids[i] == role_ids[j]){
+                  flag = 1;
+                  break
+                }
+              }
+
+              if (flag === 1) break;
+              
+            }
+
+            if (flag === 1){
+              console.log("Role IDs must be numbers > 0 and not duplicated. Try again.")
+              continue
+            }else{
+              break
+            }
+          }
+
+          const sign: boolean[] = [];
+          const spending: number[] = [];
+
+          for (let i = 0; i < role_ids.length; i++) {
+            const canSign = (await ask(`Role ${role_ids[i]} sign ability (true/false): `)).toLowerCase() === "true";
+
+            sign.push(canSign);
+
+            if (canSign) {
+              const limit = Number(await ask(`Role ${role_ids[i]} spending limit: `));
+              spending.push(limit);
+            } else {
+              spending.push(0);
+              console.log(`Role ${role_ids[i]} cannot sign: spending limit set to 0`);
+            }
+          }
+
+>>>>>>> f8ca49f (cli refining)
           const recovery = parseNumberArray(await ask("recovery_time: "));
           const users = parseStringArray(await ask("new_users: "));
           const roles = parseNumberArray(await ask("new_users_roles: "));
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> f8ca49f (cli refining)
           await createRbacWallet(
             chain,
             role_ids,
@@ -87,8 +243,15 @@ async function main() {
             users,
             roles
           );
+<<<<<<< HEAD
           break;
         }
+=======
+
+          break;
+        }
+        
+>>>>>>> f8ca49f (cli refining)
 
         case "2": {
           const chain = await askChain();
@@ -103,19 +266,95 @@ async function main() {
         }
 
         case "4": {
+<<<<<<< HEAD
           const users = parseStringArray(await ask("Users: "));
           const roles = parseNumberArray(await ask("Roles: "));
+=======
+          const currentUsersId = (await getWallet()).users.id;
+          const currentRolesId = (await getWallet()).roles_config;
+          
+          const current_roles: number[] = [];
+          for(const role of currentRolesId as any[]){
+            current_roles.push(role.key);
+          }
+          
+          const currentUsers = await getCurrentUsers(currentUsersId);
+
+          let users: string[] = [];
+          while(true){
+
+            let flag = 0;
+            users = parseStringArray(await ask("Users: "));  
+
+            for(const user of users){
+              if(currentUsers.includes(user) || !isValidSuiAddress(user)){
+                flag = 1;
+                break;
+              }
+            }
+
+            if(flag === 1){
+              console.log("Inserted address is not valid. Retry!")
+              continue
+            }else{
+              break
+            }
+          }
+
+          const roles: number[] = [];
+          for(let i = 0; i < users.length; i++){
+            while(true){
+              const role = Number(await ask(`User ${users[i]} role id: `));
+
+              if(!current_roles.includes(role)){
+                console.log("Inserted role is not valid. Retry!")
+                continue
+              }else{
+                roles.push(role)
+                break
+              }
+            }
+          }
+
+>>>>>>> f8ca49f (cli refining)
           await addUsers(users, roles);
           break;
         }
 
         case "5": {
+<<<<<<< HEAD
           const users = parseStringArray(await ask("Users to remove: "));
+=======
+          const currentUsersId = (await getWallet()).users.id;
+          const currentUsers = await getCurrentUsers(currentUsersId);
+
+
+          let users: string[] = [];
+          while(true){
+            let flag = 0
+            users = parseStringArray(await ask("Users to remove: "));
+
+            for(const user of users){
+              if(!currentUsers.includes(user) || !isValidSuiAddress(user)){
+                flag = 1
+                break
+              }
+            }
+
+            if(flag === 1){
+              console.log("Address is not part of the current wallet! Retry.");
+              continue
+            }else{
+              break
+            }
+          } 
+>>>>>>> f8ca49f (cli refining)
           await removeUsers(users);
           break;
         }
 
         case "6": {         //in questo momento la init recovery la inzia sempre 0x3cb64fc943a8af6915d682a552a90a6332f27f1fa1962603816edd8a13a101e0 in .env
+<<<<<<< HEAD
           const admin = await ask("New admin address: ");
           await initRecovery(admin);
           break;
@@ -132,16 +371,220 @@ async function main() {
         case "9": {
           const users = parseStringArray(await ask("Users: "));
           const roles = parseNumberArray(await ask("New roles: "));
+=======
+
+          const activeRecovery = (await getWallet()).active_recovery;
+
+          if(activeRecovery != null){
+            console.log("There is already an active recoovery for the current wallet!");
+            break;
+          }
+
+
+          
+          const current_admin = (await getWallet()).current_admin;
+          
+          const currentUsersId = (await getWallet()).users.id;
+          const currentUsers = await getCurrentUsers(currentUsersId);
+
+          let new_admin: string; 
+          while (true){
+            new_admin = await ask("New admin address: ");
+
+            if(!currentUsers.includes(new_admin) || !isValidSuiAddress(new_admin) || new_admin == current_admin){
+              console.log("New admin is not in the users list or its address is not valid (new admin cant be the admin itself). Retry!")
+              continue
+            }else{
+              break
+            }
+
+          }
+
+          await initRecovery(new_admin);
+          break;
+        }
+
+        case "7": {
+
+          const activeRecovery = (await getWallet()).active_recovery;
+
+          if(activeRecovery == null){
+            console.log("No active recoovery found for the current wallet!");
+            break;
+          }
+
+          await finalizeRecovery();
+          break;
+        }
+
+        case "8": {
+
+          const activeRecovery = (await getWallet()).active_recovery;
+
+          if(activeRecovery == null){
+            console.log("No active recoovery found for the current wallet!");
+            break;
+          }
+
+          await cancelRecovery();
+          break;
+        }
+        case "9": {
+
+          const currentUsersId = (await getWallet()).users.id;
+          const currentRolesId = (await getWallet()).roles_config;
+          
+          const current_roles: number[] = [];
+          for(const role of currentRolesId as any[]){
+            current_roles.push(role.key);
+          }
+          
+          const currentUsers = await getCurrentUsers(currentUsersId);
+
+
+          let users: string[] = [];
+          const roles: number[] = [];
+          while(true){
+
+            let flag = 0;
+            users = parseStringArray(await ask("Users to modify: "));
+
+            for(const user of users){
+              if(!currentUsers.includes(user) || !isValidSuiAddress(user)){
+                flag = 1
+                break
+              }
+            }
+
+            if(flag == 1){
+              console.log("Users inserted not valid! Retry!")
+              continue
+            }else{
+              break
+            }
+          }
+
+          for(let i = 0; i < users.length; i++){
+            let role: number;
+            while(true){
+              role = Number(await ask(`User ${users[i]} new role id: `));
+
+              if(!current_roles.includes(role) || isNaN(role)){
+                console.log("Role ID not in the current wallet config or is invalid! Retry!")
+                continue
+              }else{
+                roles.push(role)
+                break
+              }
+            }
+          }
+
+>>>>>>> f8ca49f (cli refining)
           await updateUsersRole(users, roles);
           break;
         }
 
         case "10": {
+<<<<<<< HEAD
           const roles = parseNumberArray(await ask("Role ids: "));
           const sign = parseBoolArray(await ask("Sign abilities: "));
           const recovery = parseNumberArray(await ask("Recovery times: "));
           const spending = parseNumberArray(await ask("Spending limits: "));
 
+=======
+
+
+          const currentRolesId = (await getWallet()).roles_config;
+          
+          const current_roles: number[] = [];
+          for(const role of currentRolesId as any[]){
+            current_roles.push(role.key);
+          }
+
+          let roles: number[] = [];
+          while(true){
+
+            let flag = 0;
+
+            roles = parseNumberArray(await ask("Role ids: "));
+            for(const role of roles){
+              if(current_roles.includes(role) || isNaN(role) || role <= 0){
+                console.log("Role already in the config or not valid! Retry!");
+                flag = 1;
+                break
+              }
+            }
+
+            if(flag == 1){
+              continue
+            }else{
+              break
+            }
+
+          }
+
+          const sign: boolean[] = [];
+          const recovery: number[] = [];
+          const spending: number[] = [];
+
+          for (const role of roles) {
+
+            console.log(`\nConfiguring role ${role}`);
+            let canSign: boolean;
+            while (true) {
+              const input = (await ask("Sign ability (true/false): ")).toLowerCase();
+
+              if (input !== "true" && input !== "false") {
+                console.log("Insert true or false");
+                continue;
+              }
+
+              canSign = input === "true";
+              break;
+            }
+
+            sign.push(canSign);
+
+    
+            if (canSign) {
+
+              while (true) {
+
+                const limit = Number(await ask("Spending limit: "));
+
+                if (isNaN(limit) || limit < 0) {
+                  console.log("Invalid spending limit!");
+                  continue;
+                }
+
+                spending.push(limit);
+                break;
+              }
+
+            } else {
+
+              spending.push(0);
+              console.log("Spending limit automatically set to 0");
+            }
+
+
+            while (true) {
+
+              const rec = Number(await ask("Recovery time: "));
+
+              if (isNaN(rec) || rec < 0) {
+                console.log("Invalid recovery time!");
+                continue;
+              } 
+
+              recovery.push(rec);
+              break;
+            }
+
+          }
+
+          console.log(roles, sign, recovery, spending)
+>>>>>>> f8ca49f (cli refining)
           await addRoles(roles, sign, recovery, spending);
           break;
         }
@@ -175,4 +618,8 @@ async function main() {
   }
 }
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> f8ca49f (cli refining)
 main();
